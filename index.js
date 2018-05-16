c7fc421fdd8a13517cb7a4836436886be5e83db4
Austin Katzer <austin484@gmail.com>
2018-05-16 20:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ function shout(string)
 
 function whisper(string)
 {
-  return string.toLower
+  return string.toLowerCase()
 }
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  var uppercase = "HELLO!"

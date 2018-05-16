ad664b61af6926b4520cfa468823b4e7418ecf08
Austin Katzer <austin484@gmail.com>
2018-05-16 20:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   if ( string === string.toUpperCase() )
-  return 'YES INDEED!'
+  return "YES INDEED!"
   
+  else if ( string === string.toLowerCase() )
+  return "I can't hear you!"
+
+    
 }
